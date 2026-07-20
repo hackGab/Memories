4634cccf6f15bbcdeed1898d783e3d142ecaa480
Gabriel-UQAT <lapg26@uqat.ca>
2026-07-20 15:30:24 -0400

    Ajout de threshold au timer pour un indice sonnore

--- MemoriesFPS/Source/MemoriesFPS/MemoriesGameMode.cpp
+++ MemoriesFPS/Source/MemoriesFPS/MemoriesGameMode.cpp
@@ -3,6 +3,8 @@
 #include "MemoriesGameMode.h"
 #include "MemoriesGameState.h"
 #include "TimerManager.h"
+//#include "Sound/SoundBase.h"
+
 
 AMemoriesGameMode::AMemoriesGameMode()
 {
@@ -25,6 +27,8 @@ void AMemoriesGameMode::BeginPlay()
 		1.0f,
 		true
 	);
+	
+	//AudioProperties();
 }
 
 void AMemoriesGameMode::TickGameTimer()
@@ -41,6 +45,14 @@ void AMemoriesGameMode::TickGameTimer()
 		
 		EndGame();
 	}
+	
+	// Quand il reste moins de TimeWarningThreshold minutes, on peut jouer un son d'indice toutes les 30 secondes (en intensifiant le pitch)
+	if (GS->TimeRemaining > 0.0f && GS->TimeRemaining <= TimeWarningThreshold && FMath::Fmod(GS->TimeRemaining, 30.0f) == 0.0f)
+		PlayTimeWarningSound(true);
+	
+	// Indice sonore à chaque tranche de X minutes (TimeWarningThreshold)
+	else if (GS->TimeRemaining > 0.0f && FMath::Fmod(GS->TimeRemaining, TimeWarningThreshold) == 0.0f)
+		PlayTimeWarningSound(false);
 }
 
 
@@ -48,3 +60,40 @@ void AMemoriesGameMode::EndGame()
 {
 	UE_LOG(LogTemp, Log, TEXT("Game Over!"));
 }
+
+void AMemoriesGameMode::PlayTimeWarningSound(bool intensify)
+{
+	
+	// Implémentez la logique pour jouer un son d'indice ici
+	
+	if (intensify)
+	{
+		UE_LOG(LogTemp, Log, TEXT("Playing intensified time warning sound."));
+		//TimeWarningAudioComponent->SetPitchMultiplier(TimeWarningSoundPitch * 1.5f); // Augmente le pitch pour intensifier le son
+		//TimeWarningAudioComponent->Play();
+	}
+	else
+	{
+		UE_LOG(LogTemp, Log, TEXT("Playing time warning sound."));
+		//TimeWarningAudioComponent->SetPitchMultiplier(TimeWarningSoundPitch); // Réinitialise le pitch
+		//TimeWarningAudioComponent->Play();
+	}
+}
+
+
+/*void AMemoriesGameMode::AudioProperties()
+{
+	if (TimeWarningSound)
+	{
+		TimeWarningAudioComponent = NewObject<UAudioComponent>(this);
+		TimeWarningAudioComponent->bAutoActivate = false;
+		TimeWarningAudioComponent->SetSound(TimeWarningSound);
+		TimeWarningAudioComponent->SetPitchMultiplier(TimeWarningSoundPitch);
+		TimeWarningAudioComponent->SetVolumeMultiplier(CurrentVolume);
+		TimeWarningAudioComponent->RegisterComponent();
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("TimeWarningSound is not assigned in the MemoriesGameMode."));
+	}
+}*/
--- MemoriesFPS/Source/MemoriesFPS/MemoriesGameMode.h
+++ MemoriesFPS/Source/MemoriesFPS/MemoriesGameMode.h
@@ -17,11 +17,32 @@ public:
 protected:
 	virtual void BeginPlay() override;
 	
-	/** La durant d'une partie, en secondes. 20min=1200sec */
+	/** La durant d'une partie, en secondes. 45min=2700sec */
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Game Timer")
-	int GameDuration = 1200;
+	int GameDuration = 2700;
+	
+	/** Indice sonore à chaque tranche de X minutes. 5min=300sec (pour la dernière tranche, à chaque 30sec le son s'intensifie)  */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Game Timer")
+	int TimeWarningThreshold = 300; // 5 minutes
+	
 	
 	static void EndGame();
+	static void PlayTimeWarningSound(bool intensify);
+	
+	
+	// TimeWarningSound properties
+	/*UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TimeWarningSound")
+	USoundBase* TimeWarningSound;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TimeWarningSound")
+	float TimeWarningSoundPitch = 1.0f;
+	
+	UPROPERTY()
+	UAudioComponent* TimeWarningAudioComponent;
+	
+	float CurrentVolume = 0.5f;
+	
+	void AudioProperties();*/
 	
 private:
 	FTimerHandle GameTimerHandle;
